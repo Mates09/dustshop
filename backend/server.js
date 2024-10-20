809ecacde2b4d352af818bda02c94beed28b4340
Mates09 <matej.snirc1@gmail.com>
2024-10-20 22:46:21 +0200

    DB setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-/* import connectDB from "./config/db.js"; */
+import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import connectDB from "./DB/db.js";
 
 const dotenvConfig = dotenv.config();
 const port = process.env.PORT || 5000;
-
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
+  connectDB();
 });
